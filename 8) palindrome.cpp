243e2ba2a3f046b9ebfb8bcbfbd8ce15fd111beb
Aakash Choudhari <aakash.choudhari.btech2024@sitpune.edu.in>
2025-07-30 15:19:04 +0530

    Create 8) palindrome.cpp

---
+++ 8) palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iospace>
+using namespace std;
+int main() {
+    char str[] = "madam";
+    int len = 0;
+    while (str[len] != '\0') len++;
+    bool isPalindrome = true;
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+    if (isPalindrome)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+    return 0;
+}
