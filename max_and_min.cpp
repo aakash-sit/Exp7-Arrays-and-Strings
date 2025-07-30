0f3583e40bc395fa30efeab2dee89a08d0c32db9
Aakash Choudhari <aakash.choudhari.btech2024@sitpune.edu.in>
2025-07-30 15:10:22 +0530

    Create max_and_min.cpp

---
+++ max_and_min.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {9, 3, 7, 1, 5};
+    int min = arr[0], max = arr[0];
+    for (int i = 1; i < 5; i++) {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+    }
+    cout << "Min: " << min << ", Max: " << max;
+    return 0;
+}
