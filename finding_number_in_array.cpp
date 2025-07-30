5f664e31306e1a7476752fe59f16dec1c700f3c3
Aakash Choudhari <aakash.choudhari.btech2024@sitpune.edu.in>
2025-07-30 15:08:13 +0530

    Create finding_number_in_array.cpp

---
+++ finding_number_in_array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {1, 3, 5, 7, 9}, num;
+    cout << "Enter number to find: ";
+    cin >> num;
+    bool found = false;
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] == num) {
+            found = true;
+            break;
+        }
+    }
+    if (found)
+        cout << "Number found.";
+    else
+        cout << "Number not found.";
+    return 0;
+}
