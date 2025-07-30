e4b5dce2cfc6921d1f6d10db7f22d53afa2013e4
Aakash Choudhari <aakash.choudhari.btech2024@sitpune.edu.in>
2025-07-30 15:12:25 +0530

    Create 5) declare_and_initialize.cpp

---
+++ 5) declare_and_initialize.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char str1[] = "Hello";
+    char str2[6] = {'W','o','r','l','d','\0'};
+    cout << str1 << " " << str2;
+    return 0;
+}
