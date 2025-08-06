c4fdc79df8013d130876b341dd62e49c7693aaf4
Aakash Choudhari <aakash.choudhari.btech2024@sitpune.edu.in>
2025-08-06 15:07:11 +0530

    Update and rename 1) printing_elements.cpp to 1) string as input.cpp

---
+++ 1) string as input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string input;
+
+    std::cout << "Enter a string: ";
+    std::cin >> input;
+    std::cout << "You entered: " << input << std::endl;
+
+    return 0;
+}
